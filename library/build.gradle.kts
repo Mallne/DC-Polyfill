99d038a8bc0557b3896ee0f141044e87c469978c
Mallne <info@mallne.cloud>
2025-03-04 22:18:26 +0100

    Updates

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -16,7 +16,7 @@ kotlin {
         publishLibraryVariants("release")
         @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_21)
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
     iosX64()
@@ -31,6 +31,7 @@ kotlin {
             }
         }
     }
+    jvmToolchain(17)
 }
 
 android {
@@ -38,6 +39,10 @@ android {
     compileSdk = libs.versions.android.compileSdk.get().toInt()
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
