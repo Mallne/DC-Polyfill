8a806f6fecb0dc609ae254f7825c0add8e536bf6
Mallne <info@mallne.cloud>
2025-08-15 09:01:48 +0200

    Restructure Synapse

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -16,7 +16,7 @@ kotlin {
         publishLibraryVariants("release")
         @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
+            jvmTarget.set(JvmTarget.JVM_21)
         }
     }
     iosX64()
@@ -41,8 +41,8 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
 
