7eb6351765ebd944064a104da34d5092abf53e08
mallne <info@mallne.cloud>
2026-09-19 10:19:39 +0200

    chore: remove GPG signing (not needed for private Nexus)

--- build.gradle.kts
+++ build.gradle.kts
@@ -97,9 +97,6 @@ mavenPublishing {
         }
     }
 
-    if (project.findProperty("dc.signingKey") != null) {
-        signAllPublications()
-    }
 
     coordinates(group.toString(), project.name)
 }
